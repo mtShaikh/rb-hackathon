e95359b9af78b44ddfdc8384ddfd657800dde358
Umair Ahmed <umairahmed515@gmail.com>
2023-07-07 13:21:54 +0500

    chore: add user settings model

--- server/app/models.py
+++ server/app/models.py
@@ -15,6 +15,7 @@ from sqlalchemy import (
     func,
 )
 from sqlalchemy.orm import relationship
+import sqlalchemy.types as types
 
 # App imports
 from app import db_manager
@@ -63,3 +64,35 @@ class User(UserMixin, Base):
 
     def __repr__(self):
         return f"<User {self.email}>"
+
+
+class ChoiceType(types.TypeDecorator):
+
+    impl = types.String
+
+    def __init__(self, choices, **kw):
+        self.choices = dict(choices)
+        super(ChoiceType, self).__init__(**kw)
+
+    def process_bind_param(self, value, dialect):
+        return [k for k, v in self.choices.items() if v == value][0]
+
+    def process_result_value(self, value, dialect):
+        return self.choices[value]
+
+
+class UserSettings(Base):
+    __tablename__ = "user_settings"
+    setting_id = Column(Integer, primary_key=True)
+    brand_title = Column(Text)
+    brand_description = Column(Text)
+    schedule = Column(
+        ChoiceType({"daily": "daily", "bi-weekly": "bi-weekly", "weekly": "weekly", "monthly": "monthly"}), nullable=False
+    )
+    post_genre = Column(Text)
+
+    def get_id(self):
+        return self.setting_id
+
+    def __repr__(self):
+        return f"<UserSettings {self.brand_title}>"
